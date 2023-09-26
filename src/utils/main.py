944b611df723defd364fb3ec5cbe892b45852ca9
Dmitri Sementsov <pluribus.zuhi@gmail.com>
2023-09-26 14:19:27 +0700

    update main.py for output result

--- src/utils/main.py
+++ src/utils/main.py
@@ -1,6 +1,13 @@
-from utils.utils import *
+from utils import *
 
-json_path = "json_dir/operations.json"
-json_full = read_json(json_path)
+# Получаем данные из operations.json
+json_full = read_json(JSON_PATH)
+
+# Сортируем полученные данные по дате (в обратном хронологическом порядке)
 parse_data(json_full)
+
+# Выводим искомый результат - 5 операций вида:
+# 14.10.2018 Перевод организации
+# Visa Platinum 7000 79** **** 6361 -> Счет **9638
+# 82771.72 руб.
 print(*result_output(output_last_five(json_full)), sep="\n\n")
